3c614d969e6413b1f96169abd3605c582a6c36c0
baisaneaishwarya <baisanehira@gmail.com>
2024-12-07 11:56:05 +0530

    form

--- config.php
+++ config.php
@@ -6,19 +6,40 @@
         private $username = "root";
         private $password = "";
         private $database = "demo";
+        private $connection;
 
         public function connect()
         {
-           $res =  mysqli_connect($this->host,$this->username,$this->password,$this->database);
-           if($res)
-           {
-            echo "Database Connect successfully !";
-           }
-           else{
-            echo "Database Connect faild !";
-           }
+           $this->connection =  mysqli_connect($this->host,$this->username,$this->password,$this->database);
+        //    if($res)
+        //    {
+        //     echo "Database Connect successfully !";
+        //    }
+        //    else{
+        //     echo "Database Connect faild !";
+        //    }
+        }
+
+        public function __construct()
+        {
+            $this->connect();
+        }
+
+        public function insert($name,$role,$salary,$age,$address,$phone)
+        { 
+            $query = "INSERT INTO employee (name,role,salary,age,address,phone)  VALUES('$name','$role',$salary,$age,'$address',$phone)";
+            $res = mysqli_query($this->connection,$query);
+            if($res)
+               {
+                echo "Database Connect successfully !";
+               }
+               else{
+                echo "Database Connect faild !";
+               }
         }
         
     }
+    // <!-- name role salary age  address phone-->
    
 ?>
+
--- index.php
+++ index.php
@@ -2,8 +2,51 @@
 
 include('config.php');
 
-$c1  = new Config();
-$c1->connect();
+$c1 = new Config();
+// $c1->connect();
 
+// echo "Database Connect successfully !";
+$is_btn_set = isset($_POST['button']);
+if($is_btn_set)
+{
+    // <!-- name role salary age  address phone -->(name,role,salary,age,address,phone) 
+    $name = $_POST['name'];
+    $role = $_POST['role'];
+    $salary = $_POST['salary'];
+    $age = $_POST['age'];
+    $address = $_POST['address'];
+    $phone = $_POST['phone'];
+
+
+    $c1->insert($name,$role,$salary,$age,$address,$phone);
+}
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration Form</title>
+</head>
+
+<body>
+  <center>
+  <h1>Employee Form</h1>
+    <form method="POST">
+        <input placeholder="Name" name="name"><br><br>
+        <input placeholder="Role" name="role"><br><br>
+        <input placeholder="Salary" name="salary"><br><br>
+        <input placeholder="Age" name="age"><br><br>
+        <input placeholder="Address" name="address"><br><br>
+        <input placeholder="Phone" name="phone"><br><br>
+        <button name = "button" type = "submit">Submit</button>
+    </form>
+</center>
+
+</body>
+
+</html>
+
